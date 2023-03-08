3405d9fb576ae5b903222ef5949d9ae1e99ef138
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-08 12:58:02 -0800

    Add login and logout requests to auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,13 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     return jsonify({'message': 'Got register request'})
+
+
+@bp.route('/login', methods=('POST'))
+def register():
+    return jsonify({'message': 'Got login request'})
+
+
+@bp.route('/logout', methods=('POST'))
+def register():
+    return jsonify({'message': 'Got logout request'})
