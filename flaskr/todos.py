fedebfce1bf95579b19d17b99b469c4111aaa49e
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-08 19:38:22 -0800

    Add new Todos table

---
+++ flaskr/todos.py
@@ -0,0 +1,9 @@
+from flaskr import db
+
+
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='todos')
+
--- flaskr/user.py
+++ flaskr/user.py
@@ -3,5 +3,6 @@ from flaskr import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password_hash = db.Column(db.String)
+    username = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    todos = db.relationship('Todos', back_populates='user')
