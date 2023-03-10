3b8060812a9abda60c3d2e64b6f7d41d85353f0c
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-09 22:20:08 -0800

    Add Todos to the same file as User

--- flaskr/todos.py
+++
@@ -1,9 +0,0 @@
-from flaskr import db
-
-
-class Todos(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', back_populates='todos')
-
--- flaskr/user.py
+++ flaskr/user.py
@@ -1,5 +1,12 @@
 from flaskr import db
 
+# TODO: Defining |Todos| in a separate file causes a circular dependency.
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='todos')
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
