be687e167a33661bd8917352e5795c13d631fb2d
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-15 20:55:38 -0700

    App now only starts from a config file that isn't checked in

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,27 +11,35 @@ db = SQLAlchemy()
 from flaskr import todos
 from flaskr import auth
 
-def create_app(test_config=None):
+def create_local_db_if_not_present(app, dbase):
+    """
+        Creates a local SQLite3 DB file if it's not present.
+    """
+
+    # Create DB if not already present.
+    if not os.path.isfile(os.path.join(app.instance_path, app.config['DB_FILE_NAME'])):
+        with app.app_context():
+            print("Creating new DB")
+            dbase.create_all()
+
+
+def create_app():
+    """
+        Creates an app instance. We assume an "instance" folder already exists.
+    """
+
     # Create and config the app.
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev', DB_FILE_NAME='flaskr.db')
-    # Note URL should start with postgresql:// vs postgres://.
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://johndoe65535:ZleLGaC6pIDMQ40BgDYtG1YgRd2eYXEh@dpg-cg5sfkl269v5l63v3meg-a.oregon-postgres.render.com/maverick_flask_todo_db'
 
-   # Ensure the instance folder exists.
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # We always require a config file to run.
+    if not app.config.from_pyfile('config.py'):
+        raise FileNotFoundError()
 
     # Hook up the database to the app. This will be used for ORMs.
     db.init_app(app)
 
-    # Create DB if not ready.
-    if not os.path.isfile(os.path.join(app.instance_path, app.config['DB_FILE_NAME'])):
-        with app.app_context():
-            print("Creating new DB")
-            db.create_all()
+    if app.config['USE_LOCAL_DB']:
+        create_local_db_if_not_present(app, db)
 
     # Register error handlers across blueprints.
     app.register_error_handler(400, bad_request)
